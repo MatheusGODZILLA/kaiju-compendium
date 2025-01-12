998af2bbcbe59be82f619921d1020a25fea59ae6
Matheus da Silva <contagodzillajapan@gmail.com>
2025-01-12 16:11:34 -0300

    Feat: criando dataset de monstros

---
+++ src/data/Dataset.ts
@@ -0,0 +1,13 @@
+const dataset = [
+    {
+        image: 'https://godzilla.com/cdn/shop/files/1954-070.jpg?v=1687898191&width=1440',
+        name: 'GODZILLA (1954)',
+        description: 'O Godzilla de 1954 surgiu como um monstro enorme, de 50 metros de altura, com barbatanas dorsais escarpadas do pescoço à cauda. Nunca antes os espectadores haviam visto um monstro dessa magnitude e, quando Godzilla lançou seu terrível raio de calor para incendiar Tóquio, o cinema mudou para sempre.',
+        height: '50',
+        weight: '20,000',
+        era: 'Showa',
+        roar: require(''),
+    },
+];
+
+export default dataset;
