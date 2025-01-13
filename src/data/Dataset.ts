da3b4aecb6097878aa000b6e9223ae494a1f0777
Matheus da Silva <contagodzillajapan@gmail.com>
2025-01-12 21:09:04 -0300

    Feat: atualizando imagem

--- src/data/Dataset.ts
+++ src/data/Dataset.ts
@@ -1,6 +1,6 @@
 const dataset = [
     {
-        image: 'https://godzilla.com/cdn/shop/files/1954-070.jpg?v=1687898191&width=1440',
+        image: 'https://godzilla.com/cdn/shop/files/1954-087.jpg?v=1687898191&width=1440',
         name: 'GODZILLA (1954)',
         description: 'O Godzilla de 1954 surgiu como um monstro enorme, de 50 metros de altura, com barbatanas dorsais escarpadas do pescoço à cauda. Nunca antes os espectadores haviam visto um monstro dessa magnitude e, quando Godzilla lançou seu terrível raio de calor para incendiar Tóquio, o cinema mudou para sempre.',
         height: '50',
