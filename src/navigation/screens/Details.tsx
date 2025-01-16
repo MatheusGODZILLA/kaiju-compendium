a2373b9df3aaa8c07b5839c0e047a8a7df82c556
Matheus da Silva <contagodzillajapan@gmail.com>
2025-01-15 22:05:44 -0300

    Feat: adicionando fundo e subtítulo na tela de detalhes do monstro

--- src/navigation/screens/Details.tsx
+++ src/navigation/screens/Details.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { Text, Image, StyleSheet, ScrollView, View, ImageBackground } from 'react-native';
 import { Audio } from 'expo-av';
 import { Accelerometer } from 'expo-sensors';
 import MonsterStats from '../../components/MonsterStats';
+
+const background = require('../../assets/footer_bg (1).png');
 
 export function Details({ route }: any) {
   const { monster } = route.params;
@@ -24,10 +26,17 @@ export function Details({ route }: any) {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image source={{ uri: monster.image }} style={styles.image} />
       <Text style={styles.name}>{monster.name}</Text>
+      <Image source={{ uri: monster.image }} style={styles.image}/>
+      <Text style={styles.subtitle}>{monster.subtitle}</Text>
       <Text style={styles.description}>{monster.description}</Text>
       <MonsterStats monster={monster} />
+
+      <View style={styles.roarContainer}>
+        <ImageBackground source={background} resizeMode="cover" style={styles.background}>
+            <Text style={styles.roar}>🔊  Agite o telefone para ouvir o rugido</Text>
+        </ImageBackground>
+      </View>     
     </ScrollView>
   );
 }
@@ -47,7 +56,7 @@ const styles = StyleSheet.create({
     width: 320, 
     height: 320, 
     borderRadius: 16, 
-    marginBottom: 20,
+    marginBottom: 15,
     borderWidth: 2,
     borderColor: '#000',
   },
@@ -55,13 +64,20 @@ const styles = StyleSheet.create({
     fontSize: 28, 
     fontWeight: 'bold', 
     color: '#333',
-    marginBottom: 10 
+    marginBottom: 15 
+  },
+  subtitle: {
+    fontSize: 22,
+    color: '#555',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   description: { 
-    fontSize: 18, 
-    textAlign: 'center', 
+    fontSize: 16, 
+    textAlign: 'justify', 
     color: '#666',
-    marginVertical: 10 
+
   },
   detailsContainer: {
     width: '100%',
@@ -80,4 +96,23 @@ const styles = StyleSheet.create({
     color: '#333', 
     fontWeight: '500' 
   },
+  roarContainer: {
+    width: '100%',
+    height: 50, 
+    borderRadius: 50,
+    overflow: 'hidden',
+    marginTop: 20,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  roar: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 12
+  }
 });
